46b19b12d8b4512e6c8189294c75f8e2dfe1c878
robin l'hyver <robinlhyver@gmail.com>
2025-05-30 18:12:14 +0100

    Add web server and 3D visualization for sensor data readings

--- Self balancing robot/src/main.cpp
+++ Self balancing robot/src/main.cpp
@@ -1,5 +1,4 @@
-#include <Wire.h>
-#include <MPU6050.h>
+#include "main.h"
 
 // global variable 
 float* RateRoll = new float(0);
@@ -21,6 +20,16 @@ float* globalAngleRoll = new float(0);
 
 unsigned long last_time =0;
 unsigned long now = 0;
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
 
 
 void gyro_signal(){
@@ -77,15 +86,102 @@ void gyro_signal(){
 
 }
 
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}
+
+String getGyroReadings(){
+    // Utilise les variables globales RateRoll, RatePitch et RateYaw et ne relit pas les valeurs du gyroscope
+    readings["gyroX"] = String(*RateRoll);
+    readings["gyroY"] = String(*RatePitch);
+    readings["gyroZ"] = String(*RateYaw);
+
+    String jsonString = JSON.stringify(readings);
+    return jsonString;
+}
+
+String getAccReadings(){
+    // Utilise les variables globales AccX, AccY et AccZ et ne relit pas les valeurs de l'accéléromètre
+    readings["accX"] = String(*AccX);
+    readings["accY"] = String(*AccY);
+    readings["accZ"] = String(*AccZ);
+
+    String jsonString = JSON.stringify(readings);
+    return jsonString;
+}
+
+String getTemperature(){
+    // Retourne toujours la valeur "5" pour la température
+    readings["temperature"] = "5";
+    String jsonString = JSON.stringify(readings);
+    return jsonString;
+}
+
 void setup() {
-    Serial.begin(115200);
-    Wire.setClock(400000);
-    Wire.begin();
-    delay(250);
-    Wire.beginTransmission(0x68);
-    Wire.write(0x6B);
-    Wire.write(0x00);
-    Wire.endTransmission();
+  // Handle Web Server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    RateRoll=0;
+    RatePitch=0;
+    RateYaw=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+
+  server.on("/resetX", HTTP_GET, [](AsyncWebServerRequest *request){
+    RateRoll=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetY", HTTP_GET, [](AsyncWebServerRequest *request){
+    RatePitch=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetZ", HTTP_GET, [](AsyncWebServerRequest *request){
+    RateYaw=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  server.begin();
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000);
+  
+
+  delay(250);
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+    
 }
 
 void loop() {
@@ -106,5 +202,21 @@ void loop() {
     Serial.println("   ");
 
     delay(50); // Court délai pour la stabilité
+
+  if ((millis() - lastTime) > gyroDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
+    lastTime = millis();
+  }
+  if ((millis() - lastTimeAcc) > accelerometerDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
+    lastTimeAcc = millis();
+  }
+  if ((millis() - lastTimeTemperature) > temperatureDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getTemperature().c_str(),"temperature_reading",millis());
+    lastTimeTemperature = millis();
+  }
 
 }
--- Self balancing robot/src/main.h
+++ Self balancing robot/src/main.h
@@ -2,14 +2,25 @@
 #define MAIN_H
 
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include <Wire.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Arduino_JSON.h>
+#include "LittleFS.h"
 // Constantes MPU
 #define MPU_ADDR 0x68
 #define ESP_ADDRESS 0x75
-// Déclarations des fonctions
-uint8_t readMPURegister(uint8_t reg);
-int myFunction(int x, int y);
 
+const char* ssid = "TelRobin";
+const char* password = "robinestbeau";
 
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long lastTimeTemperature = 0;
+unsigned long lastTimeAcc = 0;
+unsigned long gyroDelay = 10;
+unsigned long temperatureDelay = 1000;
+unsigned long accelerometerDelay = 200;
 
 #endif
